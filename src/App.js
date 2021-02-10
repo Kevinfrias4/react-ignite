dc14bc48e7a0caddbc1327011cd9a58a12b086d7
kevin frias <kevinfrias@ymail.com>
2021-02-10 14:29:58 -0600

    Added global styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 //Components and pages
 import Home from './pages/Home';
+import GlobalStyles from './components/GlobalStyles';
 
 function App() {
 
 
   return (
     <div className="App">
+      <GlobalStyles />
       <Home />
     </div>
   );
