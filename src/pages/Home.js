7b225a608bf442eecf9c5bc7f4dd925c11ea538c
kevin frias <kevinfrias@ymail.com>
2021-02-10 15:43:28 -0600

    Adding game detail data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import GameDetail from '../components/GameDetail';
 //Redux
 import { useDispatch, useSelector } from 'react-redux';
 import { loadGames } from '../actions/gamesAction';
@@ -22,6 +23,7 @@ const Home = () => {
 
     return(
         <GameList>
+            <GameDetail />
             <h2>Upcoming Games</h2>
             <Games>
                 {upcoming.map(game => (
