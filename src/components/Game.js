e76e2cac76d48f66b53e25e5332662cc1a5d2229
kevin frias <kevinfrias@ymail.com>
2021-02-11 13:00:54 -0600

    Added gamedetail toggling

--- src/components/Game.js
+++ src/components/Game.js
@@ -12,6 +12,7 @@ const Game = ({name, released, image, id}) => {
     //Load Details
     const dispatch = useDispatch();
     const loadDetailHandler = () => {
+        document.body.style.overflow = 'hidden';
         dispatch(loadDetail(id));
     }
 
@@ -31,6 +32,8 @@ const StyledGame = styled(motion.div)`
     box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.2);
     text-align: center;
     border-radius: 1rem;
+    cursor: pointer;
+    overflow: hidden;
     img {
         width: 100%;
         height: 40vh;
