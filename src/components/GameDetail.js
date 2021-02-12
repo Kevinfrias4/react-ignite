8b3d7e6bd15ee463d5a87cea4483fc53083ef49b
kevin frias <kevinfrias@ymail.com>
2021-02-12 14:35:39 -0600

    Added stars for rating

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -13,6 +13,9 @@ import xbox from '../img/xbox.svg';
 import nintendo from '../img/nintendo.svg';
 import apple from '../img/apple.svg';
 import gamepad from '../img/gamepad.svg';
+//Star Images
+import starEmpty from '../img/star-empty.png';
+import starFull from '../img/star-full.png';
 
 const GameDetail = ({ pathId }) => {
 
@@ -24,6 +27,20 @@ const GameDetail = ({ pathId }) => {
             document.body.style.overflow = 'auto';
             history.push('/');
         }
+    }
+
+    //Get Stars
+    const getStars = () => {
+        const stars = [];
+        const rating = Math.floor(game.rating);
+        for (let i = 1; i <= 5; i++) {
+            if (i <= rating) {
+                stars.push(<img alt="star" key={i} src={starFull}></img>)
+            } else {
+                stars.push(<img alt="star" key={i} src={starEmpty}></img>)
+            }
+        }
+        return stars;
     }
 
     //Get Platform Images
@@ -55,6 +72,7 @@ const GameDetail = ({ pathId }) => {
                             <div className="rating">
                                 <motion.h3 layoutId={`title ${pathId}`}>{game.name}</motion.h3>
                                 <p>Rating: {game.rating}</p>
+                                {getStars()}
                             </div>
                             <Info>
                                 <h3>Platforms</h3>
@@ -129,6 +147,11 @@ const Stats = styled(motion.div)`
     display: flex;
     align-items: center;
     justify-content: space-between;
+    img {
+        width: 2rem;
+        height: 2rem;
+        display: inline;
+    }
 `;
 
 const Info = styled(motion.div)`
